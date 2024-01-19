705221f41e233877b33790c3b7f7c46878495951
Igor Brusko <tailsmiles2015b@gmail.com>
2024-01-20 03:37:18 +0700

    Начал делать UI

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,7 @@
+public class Panel<T> : BasePanel where T : BasePanelSettings
+{
+    public virtual void Setup(T settings)
+    {
+         
+    }
+}
---
+++ Assets/Scripts/StatsPanel.cs
@@ -0,0 +1,12 @@
+public class StatsPanel : Panel<StatsPanelSettings>
+{
+    public override void Setup(StatsPanelSettings settings)
+    {
+        base.Setup(settings);
+    }
+}
+
+public class StatsPanelSettings : BasePanelSettings
+{
+    
+}
