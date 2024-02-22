e6860b02e838fc2dc92de20f6ad77e42d7ea832c
Igor Brusko <tailsmiles2015b@gmail.com>
2024-02-22 16:50:30 +0700

    Промежуточный попапа выбора предметов

--- Assets/Scripts/SelectItemPopup.cs
+++ Assets/Scripts/SelectItemPopup.cs
@@ -1,13 +1,29 @@
 using System;
+using System.Linq;
+using UnityEngine;
 
 public class SelectItemPopup : Popup<SelectItemPopupSetting>
 {
+    [SerializeField] private Transform _itemsParent; 
+    
     public override void Setup(SelectItemPopupSetting setting)
     {
-        
+        var inventory = setting.Player.Inventory;
+        var items = inventory.Items.Where(x => x.Setting.ItemCategoryType == setting.ItemCategoryType);
+
+        var itemPrefab = SettingsProvider.Get<PrefabSettings>().InventoryElement;
+
+        foreach (var item in items)
+        {
+            var itemObject = Instantiate(itemPrefab, _itemsParent);
+            itemObject.Setup(item);
+        }
     }
 }
 
 public class SelectItemPopupSetting : BasePopupSettings
 {
+    public ItemCategoryType ItemCategoryType;
+    public Player Player;
+    public EquipmentReserveManager EquipmentReserveManager;
 }
